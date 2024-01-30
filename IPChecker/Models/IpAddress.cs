25c04b9f928e6248de746e9ab3da243e60bfa7ae
filippos dimitriadis <fildimitriadis1998@gmail.com>
2024-01-30 23:41:06 +0200

    scaffold db

---
+++ IPChecker/Models/IpAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IPChecker.Models;
+
+public partial class IpAddress
+{
+    public int Id { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Ip { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ IPChecker/Models/IpcheckerDbContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPChecker.Models;
+
+public partial class IpcheckerDbContext : DbContext
+{
+    public IpcheckerDbContext()
+    {
+    }
+
+    public IpcheckerDbContext(DbContextOptions<IpcheckerDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<IpAddress> IpAddresses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LOCALHOST\\SQLEXPRESS;Database=IPCHECKER_DB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.ToTable("COUNTRIES");
+
+            entity.HasIndex(e => e.Name, "IX_COUNTRIES_NAME").IsUnique();
+
+            entity.HasIndex(e => e.ThreeLetterCode, "IX_COUNTRIES_THREELETTERCODE").IsUnique();
+
+            entity.HasIndex(e => e.TwoLetterCode, "IX_COUNTRIES_TWOLETTERCODE").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.CreatedAt).HasColumnName("CREATED_AT");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("NAME");
+            entity.Property(e => e.ThreeLetterCode)
+                .HasMaxLength(3)
+                .IsFixedLength()
+                .HasColumnName("THREE_LETTER_CODE");
+            entity.Property(e => e.TwoLetterCode)
+                .HasMaxLength(2)
+                .IsFixedLength()
+                .HasColumnName("TWO_LETTER_CODE");
+        });
+
+        modelBuilder.Entity<IpAddress>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_IPADDRESSES");
+
+            entity.ToTable("IP_ADDRESSES");
+
+            entity.HasIndex(e => e.Ip, "IX_IPADDRESSES_IP").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.CountryId).HasColumnName("COUNTRY_ID");
+            entity.Property(e => e.CreatedAt).HasColumnName("CREATED_AT");
+            entity.Property(e => e.Ip)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("IP");
+            entity.Property(e => e.UpdatedAt).HasColumnName("UPDATED_AT");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.IpAddresses)
+                .HasForeignKey(d => d.CountryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_IPADDRESSES_COUNTRIES");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
